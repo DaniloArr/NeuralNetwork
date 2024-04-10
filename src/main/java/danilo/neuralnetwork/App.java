5e2671f95809e90aa84ce302e71fcff70ef4e77b
DaniloArr <danilo.arreb@gmail.com>
2024-04-09 22:30:38 -0300

    Function AND - Logic Gate

--- src/main/java/danilo/neuralnetwork/App.java
+++ src/main/java/danilo/neuralnetwork/App.java
@@ -1,28 +1,64 @@
 package danilo.neuralnetwork;
 
+import java.util.Iterator;
+
+// @formatter:off
 /**
- * Hello world!
- *
+ * LOGIC GATE:
+ * INPUT	  AND  NAND    OR	XOR		NOR	  XNOR 
+ * 	0 0		-> 0	1		0	 0		 1	   1
+ * 	0 1		-> 0	1		1	 1		 0	   0
+ * 	1 0		-> 0	1		1	 1		 0	   0
+ * 	1 1		-> 1	0		1	 0		 0	   1
  */
+
+//@formatter:on
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        double[] inputValue = {0, 1};        
-        double[] weightValue = {0.5, 0.5};
+	static double neuron(double[] x, double[] w, double b) {
+		//wighted sum
+        double z = 0.0;
         
-        double bias = 0.5; // tendencia
-        
-        double sum = 0.0;
-        
-        for(int i = 0; i < inputValue.length; i++) {
-        	sum += inputValue[i] * weightValue[i];
+        for(int i = 0; i < x.length; i++) {
+        	z += x[i] * w[i];
         }
         
-        sum += bias;
+        z += b;
         
-        double a = sum > 0 ? 1.0 : 0.0;
-        
-        System.out.println(a);
+        //activation function
+        return z > 0 ? 1.0 : 0.0;
+	}
+	
+	static double and(double x1, double x2) {
+		return neuron(new double[] {x1, x2}, new double[] {1, 1}, -0);
+	}
+	
+	static double or(double x1, double x2) {
+		return neuron(new double[] {x1, x2}, new double[] {1, 1}, 0);
+	}
+	
+    public static void main( String[] args ) {
+    	System.out.println("AND");
+    	for (int i = 0; i < 4; i++) {
+			double x1 = i/2;
+			double x2 = i%2;
+			
+			double output = and(x1, x2);
+			
+			System.out.printf("%d%d\t%d\n", (int) x1, (int) x2, (int) output);
+			
+		}
+    	
+    	System.out.println("\nOR");
+    	for (int i = 0; i < 4; i++) {
+			double x1 = i/2;
+			double x2 = i%2;
+			
+			double output = or(x1, x2);
+			
+			System.out.printf("%d%d\t%d\n", (int) x1, (int) x2, (int) output);
+			
+		}
     }
 }
